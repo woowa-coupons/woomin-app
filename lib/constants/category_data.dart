b4e8f2172e3020a965b3d8d86820059911a0b1f3
litae <109706689+qkdflrgs@users.noreply.github.com>
2023-11-15 22:46:37 +0900

    feat: 카테고리 상수 추가 (#41)

---
+++ lib/constants/category_data.dart
@@ -0,0 +1,32 @@
+const List<List<Map<String, String>>> categoryData = [
+  [
+    {
+      'title': '알뜰배달',
+      'urlPath': '/soon',
+      'iconPath': 'assets/icons/delivery.png'
+    },
+    {
+      'title': '돈까스∙회',
+      'urlPath': '/soon',
+      'iconPath': 'assets/icons/japanese.png'
+    },
+    {'title': '피자', 'urlPath': '/soon', 'iconPath': 'assets/icons/pizza.png'},
+    {'title': '중식', 'urlPath': '/soon', 'iconPath': 'assets/icons/chinese.png'},
+    {'title': '치킨', 'urlPath': '/soon', 'iconPath': 'assets/icons/chicken.png'}
+  ],
+  [
+    {'title': '버거', 'urlPath': '/soon', 'iconPath': 'assets/icons/burger.png'},
+    {'title': '분식', 'urlPath': '/soon', 'iconPath': 'assets/icons/korean.png'},
+    {
+      'title': '디저트',
+      'urlPath': '/soon',
+      'iconPath': 'assets/icons/dessert.png'
+    },
+    {'title': 'W마트', 'urlPath': '/soon', 'iconPath': 'assets/icons/mart.png'},
+    {
+      'title': '홈플러스',
+      'urlPath': '/soon',
+      'iconPath': 'assets/icons/homeplus.png'
+    }
+  ]
+];
